dc482d814b85570a81bd81fc084cb566bd1d9c22
DavidovichD <116546681+DavidovichD@users.noreply.github.com>
2022-11-05 15:16:05 +0200

    Initial commit

---
+++ js/JSON.js
@@ -0,0 +1,16 @@
+﻿const ingredientJson = [
+    {
+        id: 1,
+        name: 'Broccoli',
+
+        image: 'https://www.health.harvard.edu/media/content/images/p7_Broccoli_HH1812_gi905351392.jpg',
+        calories: 34
+    },
+    {
+        id: 2,
+        name: 'Cucumber',
+
+        image: 'https://www.lipmanfamilyfarms.com/wp-content/uploads/2020/09/Cucumber-hero@2x-1024x696.png',
+        calories: 25
+    }
+]
